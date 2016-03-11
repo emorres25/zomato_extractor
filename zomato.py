bb3e730c87d1823a01ad6c410f696faf20d06b0b
emorres25 <emorres25@gmail.com>
2016-03-12 01:39:21 +0530

    Huge Improvements
    Added some text graphics, and a lot of information.
    A memorable project.

--- zomato.py
+++ zomato.py
@@ -1,16 +1,23 @@
 import requests
 import urllib2
 import os
+import json
+from tabulate import tabulate
 
+rest_name = []
+rest_locality = []
+rest_code = []
+rest_avg_cost = []
+rest_rating = []
 query = raw_input("Enter the name of the location: ").split(' ')
 initial_url = 'https://developers.zomato.com/api/v2.1/locations?query='
 for i in range (0,len(query)):
 	if(i==len(query)-1):
-		initial_url = initial_url + query[i] + '&count=6'
+		initial_url = initial_url + query[i] + '&count=6&sort=rating'
 	else:
 		initial_url = initial_url + query[i] + '%20'
 
-print initial_url
+#print initial_url
 
 api_key = '22b2050b2c779d35f5da049e8d414fcd'
 url = 'https://developers.zomato.com/api/v2.1/locations?query=janakpuri&count=6'
@@ -39,5 +46,45 @@ response = requests.get(url_ld, headers = data_location_details)
 location_details_stuff = response.json()
 
 
-print location_details_stuff['best_rated_restaurant'][0]['restaurant']['name']
-print location_details_stuff['best_rated_restaurant'][0]['restaurant']['location']['address']
+#print location_details_stuff['best_rated_restaurant'][0]['restaurant']['name']
+#print location_details_stuff['best_rated_restaurant'][0]['restaurant']['location']['address']
+
+
+
+initial_search_url = 'https://developers.zomato.com/api/v2.1/search?q='
+for i in range(0, len(query)):
+	if(i == len(query)-1):
+		initial_search_url = initial_search_url + query[i] + '&count=10&radius=5.2&sort=real_distance'
+	else:
+		initial_search_url = initial_search_url + query[i] + '%20'
+
+#print initial_search_url
+
+
+data_search = {'Accept' : 'application/json', 'user-key' : '22b2050b2c779d35f5da049e8d414fcd', 'User-Agent' : 'curl/7.35.0'}
+response = requests.get(initial_search_url, headers = data_search)
+
+#print response.text
+
+
+resp_dict = json.loads(response.text)
+for i in range(0,9):
+	#print resp_dict['restaurants'][i]['restaurant']['name']
+	rest_name.append(resp_dict['restaurants'][i]['restaurant']['name'])
+	rest_locality.append(resp_dict['restaurants'][i]['restaurant']['location']['locality'])
+	rest_code.append(resp_dict['restaurants'][i]['restaurant']['R']['res_id'])
+	rest_avg_cost.append(resp_dict['restaurants'][i]['restaurant']['average_cost_for_two'])
+	rest_rating.append(resp_dict['restaurants'][i]['restaurant']['user_rating']['aggregate_rating'])
+#print rest_name
+#print rest_locality
+#print rest_code
+#print rest_avg_cost
+
+headers = ["Name","Locality","Code","Budget","Rating"]
+#table = [[rest_name[0], rest_locality[0], rest_code[0], rest_avg_cost[0]]]
+table = []
+
+for i in range(0,9):
+	table.insert(i,[rest_name[i],rest_locality[i],rest_code[i],rest_avg_cost[i], rest_rating[i]]) 
+print tabulate(table, headers, tablefmt="rst", stralign='left')
+
